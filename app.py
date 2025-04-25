d115bdb65f952eec49c67657fa8628cb3158621f
Kelsey Bowen <bowenke@classwork.engr.oregonstate.edu>
2025-04-25 14:03:06 -0700

    initial setup

---
+++ app.py
@@ -0,0 +1,63 @@
+# ########################################
+# ########## SETUP
+
+from flask import Flask, render_template, request, redirect
+import database.db_connector as db
+
+PORT = 8282
+
+app = Flask(__name__)
+
+# ########################################
+# ########## ROUTE HANDLERS
+
+# READ ROUTES
+@app.route("/", methods=["GET"])
+def home():
+    try:
+        return render_template("home.j2")
+
+    except Exception as e:
+        print(f"Error rendering page: {e}")
+        return "An error occurred while rendering the page.", 500
+
+
+@app.route("/bsg-people", methods=["GET"])
+def bsg_people():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = "SELECT bsg_people.id, bsg_people.fname, bsg_people.lname, \
+            bsg_planets.name AS 'homeworld', bsg_people.age FROM bsg_people \
+            LEFT JOIN bsg_planets ON bsg_people.homeworld = bsg_planets.id;"
+        query2 = "SELECT * FROM bsg_planets;"
+        people = db.query(dbConnection, query1).fetchall()
+        homeworlds = db.query(dbConnection, query2).fetchall()
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "bsg-people.j2", people=people, homeworlds=homeworlds
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+
+
+# ########################################
+# ########## LISTENER
+
+if __name__ == "__main__":
+    app.run(
+        port=PORT, debug=True
+    )  # debug is an optional parameter. Behaves like nodemon in Node.
